854edd4dd33341436d9e657b6b4a506d43fa5e83
Mang Kipgen <65645072+MangKipgen@users.noreply.github.com>
2025-06-13 17:50:40 -0400

    initial edit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const retroButton = document.getElementById('myRetroButton');
 
     retroButton.addEventListener('click', () => {
-        alert('Hello from the early 2000s web!');
+        alert('Hello');
         // You'll add your subtle modern interactions here later!
     });
 });
